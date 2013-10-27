8801137cdbbdd71b58615e79959ff09e99fc70b8
Deepak Mani <deepakmani@Deepaks-MacBook-Air.local>
2013-10-27 07:25:28 +0530

    Initialize Repository

---
+++ app/helpers/nav_helper.rb
@@ -0,0 +1,12 @@
+module NavHelper
+  def nav_tab (title, url, options = {})
+    current_tab = options.delete(:current)
+    options[:class] = (current_tab == title) ? 'active' : 'inactive'
+    content_tag(:li, link_to(title, url), options)
+  end
+
+  def currently_at(tab)
+    render partial: 'layouts/header', locals: {current_tab: tab}
+  end
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShankGlobal::Application.config.secret_key_base = 'c98d23bc7f6706364a8bfdfde0f95fb96a0513e4781d87390771cef4e3c84fdf33278612d99622b5ee9f03715249003bc0b50b95c554823c085739be67b4df9e'
